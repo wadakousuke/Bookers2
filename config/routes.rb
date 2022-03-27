81f2bce0206bced7a795d7ab0f9977d04637d458
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-03-27 14:53:34 +0000

    モデル、コントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 
 
+  get 'users/show'
+  get 'users/edit'
+  get 'users/index'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  devise_for :users
   root to: "homes#top"
   get "home/about" => "homes#about", as:"about"
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books
   resources :users, only: [:show, :edit, :index, :update]
